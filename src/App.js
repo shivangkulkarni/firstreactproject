32618d9667edcce2e7e7babf197577da0bca8321
Shivang Kulkarni <shivangk888@gmail.com>
2024-06-26 15:54:15 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
+import MovieCard from './MovieCard';
+
+import React from 'react';
+
 import './App.css';
+import SearchIcon from './search.svg';
+
+// f96a246
+const API_URL = 'http://www.omdbapi.com?apikey=f96a246';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies(''); // display all movies
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div classname="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ?   (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+
+                    </div>
+                ) : (
+                    <div>
+                        <h2>No Movies Found</h2>
+                    </div>
+                )}
+        </div>
+    );
 }
+
 
 export default App;
